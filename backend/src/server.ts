6830af86acaef3aba39f11185cc05443d60256e2
YerosenHunduma <yerosenhunduma@gmail.com>
2025-03-06 04:29:47 +0300

    add cors and cookie-parser middlware on server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import { config } from 'dotenv';
 import { connectDb } from './config/Dbconfig';
 import routeRoute from './routes';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 config();
 
 const PORT = process.env.PORT || 5000;
@@ -11,8 +12,14 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.use(cors());
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true
+    })
+);
 
 app.use('/api', routeRoute);
 
