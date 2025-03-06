00e7e417ed0c4af96b8be93779acc31e94a542e8
YerosenHunduma <yerosenhunduma@gmail.com>
2025-03-06 04:26:51 +0300

    create controller for auth endpoints

--- backend/src/controller/auth.controller.ts
+++ backend/src/controller/auth.controller.ts
@@ -1,5 +1,44 @@
+import bcrypt from 'bcryptjs';
+import { errorHandler } from './../utils/errorHandler';
 import catchAsyncError from '../middleware/catchAsyncError';
+import userModel from '../model/user/user.model';
+import { hashedPassword } from '../utils/hashPassword';
+import { validationResult } from 'express-validator';
+import { issueJWT } from '../utils/signToken';
 
-export const signup = catchAsyncError(async (req, res, next) => {});
+export const signup = catchAsyncError(async (req, res, next) => {
+    const { username, password, role } = req.body;
 
-export const login = catchAsyncError(async (req, res, next) => {});
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+        const errorMessage = error.array().map((err) => err.msg);
+        return next(new errorHandler(errorMessage[0], 400));
+    }
+
+    const user = await userModel.findOne({ username });
+    if (user) return next(new errorHandler('User already exists', 400));
+
+    const hashedP = await hashedPassword(password);
+
+    new userModel({ username, password: hashedP, role }).save();
+
+    return res.status(201).json({ message: 'User registered successfully' });
+});
+
+export const login = catchAsyncError(async (req, res, next) => {
+    const { username, password } = req.body;
+
+    const user = await userModel.findOne({ username });
+
+    if (!user) return next(new errorHandler('Invalid credentials', 400));
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) return next(new errorHandler('Invalid credentials', 400));
+
+    const token = issueJWT(user.id, user.role);
+
+    res.cookie('access_token', token, { httpOnly: true, sameSite: 'none', maxAge: 24 * 60 * 60 * 1000 })
+        .status(200)
+        .json(user);
+});
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import authRoute from './auth.routes';
+import ticketRoute from './ticket.routes';
+import globalErrorHandler from '../middleware/globalErrorHandler';
 
 const router = Router();
 
 router.use('/auth', authRoute);
+router.use('/ticket', ticketRoute);
+router.use(globalErrorHandler);
 
 export default router;
